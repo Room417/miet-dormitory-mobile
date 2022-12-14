2cef66fc0411d6b4bd28866b500e7d347f5f6c5a
Maxim Sergeev <mugz3m.others@gmail.com>
2022-12-14 18:25:50 +0300

    Move MainActivity in view.ui package

--- app/src/main/java/ru/miet/dormitory/ui/view/MainActivity.kt
+++ app/src/main/java/ru/miet/dormitory/ui/view/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.miet.dormitory
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -10,7 +11,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.miet.dormitory.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,13 +20,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
+        setupBottomNavigationView(navView)
+    }
 
+    private fun setupBottomNavigationView(navView: BottomNavigationView) {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
         val navController = navHostFragment.navController
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_store,
@@ -36,5 +36,12 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.navigation_login -> navView.visibility = View.GONE
+                else -> navView.visibility = View.VISIBLE
+            }
+        }
     }
 }
